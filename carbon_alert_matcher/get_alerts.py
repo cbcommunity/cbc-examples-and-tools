8934b02fc1b2bb4848e29f7255ed0cbf9b0dbf12
Emanuela Mitreva <emitreva@vmware.com>
2024-02-01 09:54:05 +0200

    Rename and restructure main

--- carbon_alert_matcher/get_alerts.py
+++ carbon_alert_matcher/get_alerts.py
@@ -3,12 +3,11 @@ from cbc_sdk.platform.alerts import Alert
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-import datetime
-from typing import List, Tuple
+
+from typing import List
 
 
-
-def perform_kmeans(alert_list: List[str], alert: str, num_clusters: int=4) -> List[str]:
+def perform_kmeans(alert_list: List[str], alert: str, num_clusters: int = 4) -> List[str]:
     """Perform k-means alerts.
 
     Performs the k-means algorithm on all the retrieved and grouped alerts.
@@ -24,7 +23,7 @@ def perform_kmeans(alert_list: List[str], alert: str, num_clusters: int=4) -> Li
         list[str]: list of alerts reasons that are similar to the alert
     """
     # Vectorize the alert reasons
-    vectorizer = TfidfVectorizer(stop_words='english')
+    vectorizer = TfidfVectorizer(stop_words="english")
     vectors = vectorizer.fit_transform(alert_list)
 
     # Apply K-means clustering
@@ -46,6 +45,7 @@ def perform_kmeans(alert_list: List[str], alert: str, num_clusters: int=4) -> Li
 
     # Predict the cluster for the new example
     predicted_cluster = kmeans.predict(new_example_vector)[0]
+    # return only the cluster to which the alert belongs to
     return clusters[predicted_cluster]
 
 
@@ -60,7 +60,7 @@ def load_stop_words() -> List[str]:
         return []
 
 
-def get_alerts(cb: CBCloudAPI, alert_id: str, start_time: str=None, end_time: str=None) -> Tuple[str, dict]:
+def get_alerts(alert_id: str, start_time: str = None, end_time: str = None, group_by: bool = False) -> dict:
     """Get all alerts for specific period and group them by reason.
 
     Arg:
@@ -68,12 +68,18 @@ def get_alerts(cb: CBCloudAPI, alert_id: str, start_time: str=None, end_time: st
         alert_id (str): The id for the alert, for which similar alerts are retrieved.
         start_time (str): start date for the period
         end_time (str): end date for the period
+        group_by (bool): whether to group the alerts by reason (default False)
 
     Returns:
-        tuple (str, dict): cleaned reason for alert and dictionary with keys the labels and values a list of alerts that have that reason.
+        dict: information about the similar alerts
     """
-    # get the alert and prepare it for clustering
+    # TODO - Do not hard-code
+    cb = CBCloudAPI(profile="alert_matcher")
+
+    # get stop word, which are going to be removed from the words of the text
     STOP_WORDS = load_stop_words()
+
+    # get the alert and prepare it for clustering
     alert = cb.select(Alert, ID)
     clean = " ".join([item for item in alert.reason.lower().split() if item not in STOP_WORDS])
 
@@ -82,7 +88,7 @@ def get_alerts(cb: CBCloudAPI, alert_id: str, start_time: str=None, end_time: st
     if start_time and end_time:
         alerts = alerts.add_time_criteria("backend_update_timestamp", start=start_time, end=end_time)
 
-    # create the mappings
+    # create the mappings, get the unique reasons, so that the algorithm works on smaller set of records
     alerts_mappings = {}
     for alert in alerts:
         clean = " ".join([item for item in alert.reason.lower().split() if item not in STOP_WORDS])
@@ -90,44 +96,24 @@ def get_alerts(cb: CBCloudAPI, alert_id: str, start_time: str=None, end_time: st
             alerts_mappings[clean] = [alert]
         else:
             alerts_mappings[clean].append(alert)
-    return clean, alerts_mappings
 
-
-if __name__ == "__main__":
-    cb = CBCloudAPI()
-    # TO DELETE
-    ID = "6f648cfb-3bbf-d434-0cd9-5b47f38ed5ad"
-    timestamp = datetime.datetime.utcnow()
-    start_time = "{}Z".format(timestamp.isoformat())
-    end_time = start_time
-    start_time = "2024-01-10T11:38:39.767683Z"
-    GROUP = True
-
-    clean, alerts_mappings = get_alerts(cb, ID, start_time, end_time)
+    # use k-means to cluster the unique reasons
     cluster_texts = perform_kmeans(alerts_mappings.keys(), clean)
-    # 1.  id -> type, severity reason
-    # 2.  index -> reason, alert_list [id -> type severity reason]
-    if not GROUP:
-        similar_alerts = {}
-        for label in cluster_texts:    
-            curr_alerts = alerts_mappings.get(label)
-            for al in curr_alerts:
-                similar_alerts[al.id] = dict(type=al.type,
-                                             severity=al.severity,
-                                             reason=al.reason)
 
-        print(f"Checking for {clean}, found: {len(similar_alerts)} similar.")
-    else:
-        similar_alerts = {}
-        index = 0
-        for label in cluster_texts:    
+    # prepare the data to be displayed
+    similar_alerts = {}
+    if not group_by:
+        for label in cluster_texts:
             curr_alerts = alerts_mappings.get(label)
-            similar_alerts[index] = dict(reason = curr_alerts[0].reason, alerts = dict())
             for al in curr_alerts:
-                similar_alerts[index]["alerts"][al.id] = dict(type=al.type,
-                                                              severity=al.severity,
-                                                              reason=al.reason)
+                similar_alerts[al.id] = dict(type=al.type, severity=al.severity, reason=al.reason)
+    else:
+        index = 0
+        for label in cluster_texts:
+            curr_alerts = alerts_mappings.get(label)
+            similar_alerts[index] = dict(reason=curr_alerts[0].reason, alerts=dict())
+            for al in curr_alerts:
+                similar_alerts[index]["alerts"][al.id] = dict(type=al.type, severity=al.severity, reason=al.reason)
             index += 1
 
-        print(f"Checking for {clean}, found: {len(similar_alerts)} similar grouped.")
-        print(similar_alerts)
+    return similar_alerts
