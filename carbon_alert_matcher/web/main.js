b0c4a869167e260ecb5783aac5ef8eba3e0f038f
Hristo Karagitliev <hkaragitliev@vmware.com>
2024-02-02 00:09:14 +0200

    remove dead code

--- carbon_alert_matcher/web/main.js
+++ carbon_alert_matcher/web/main.js
@@ -1,27 +1,12 @@
-var import_org_data_set = false;
-var export_org_data_set = false;
-var import_data_selected = false;
-var export_data = {}
-var import_settings = {}
-var export_org_raw_data = {}
-
-async function refresh_org_data(data) {
-	let policyInfoBtnStat = false;
-	let raw_html = "";
-	if (data.orgs_count_total == 0) {
-		org_keys = "-"
-		orgs_count_total = "-"
-		policyInfoBtnStat = true;
-		raw_html = "<div class='alert alert-warning m-3 text-center' role='alert'>Import Orgs not configured.</div>"
+async function refresh_page(data, group) {
+	document.getElementById("importPolicyContainer").innerHTML = data;
+	document.getElementById("getAlertsBtn").disabled = false;
+	document.getElementById("getAlertsBtnSpinner").classList.add("d-none");
+	if (group == true) {
+		document.getElementById("alertsHeader").innerText = "Grouped Alerts - Reason"
 	} else {
-		org_keys = 'asd';
-		orgs_count_total = data.orgs_count_total;
-		policyInfoBtnStat = false;
-		raw_html = data;
+		document.getElementById("alertsHeader").innerText = "All Alerts"
 	}
-	document.getElementById("importPolicyContainer").innerHTML = raw_html;
-	document.getElementById("getImportPolicyInfoBtn").disabled = policyInfoBtnStat;
-	document.getElementById("getImportPolicyInfoBtnSpinner").classList.add("d-none");
 }
 
 async function get_alerts() {
@@ -34,18 +19,9 @@ async function get_alerts() {
 	var host_based_firewall = document.getElementById("hostBasedFirewall").checked;
 	var intrusion_detection_system = document.getElementById("intrusionDetectionSystem").checked;
 	var containers_runtime = document.getElementById("containersRuntime").checked;
-	let org_data = await eel.get_alerts(group, cb_analytics, watchlists, usb_device_control, host_based_firewall, intrusion_detection_system, containers_runtime)();
-	var parsed_data = JSON.parse(org_data);
+	let alerts = await eel.get_alerts(group, cb_analytics, watchlists, usb_device_control, host_based_firewall, intrusion_detection_system, containers_runtime)();
+	var parsed_data = JSON.parse(alerts);
 	if (parsed_data) {
-		export_org_data_set = true;
-		export_org_raw_data = parsed_data;
-		document.getElementById("getAlertsBtn").disabled = false;
-		document.getElementById("getAlertsBtnSpinner").classList.add("d-none");
-		if (group == true) {
-			document.getElementById("alertsHeader").innerText = "Grouped Alerts - Reason"
-		} else {
-			document.getElementById("alertsHeader").innerText = "All Alerts"
-		}
-		refresh_org_data(parsed_data);
+		refresh_page(parsed_data, group);
 	}
 }
