525acf782e6dbd85f7a10bc9c03d75ee0f8456fb
Hristo Karagitliev <hkaragitliev@vmware.com>
2024-02-01 15:36:57 +0200

    updates

--- carbon_alert_matcher/alert_matcher.py
+++ carbon_alert_matcher/alert_matcher.py
@@ -6,100 +6,58 @@ import eel
 
 
 @eel.expose
-def get_alerts():
+def get_alerts(match_similar, cb_analytics, watchlists, usb_device_control, host_based_firewall, intrusion_detection_system, containers_runtime):
     "text"
-    alerts = ga.get_alerts()
+    req_metadata = {
+        "match_similar": match_similar,
+        "cb_analytics": cb_analytics,
+        "watchlists": watchlists,
+        "usb_device_control": usb_device_control,
+        "host_based_firewall": host_based_firewall,
+        "intrusion_detection_system": intrusion_detection_system,
+        "containers_runtime": containers_runtime
+    }
+    alerts = ga.get_alerts(req_metadata)
 
-    # clean, alerts_info = dict()
-    print('clean', alerts, '\n')
     html = ''
-    # for alert in alerts:
-    #     html += '<div class="card small mt-0 m-3 border">\n'
-    #     html += '    <div class="card-header bg-light">\n'
-    #     html += '        <dl class="row lh-1 hr-margin-y">\n'
-    #     html += '            <dt class="col-sm-4">URL</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{alerts[import_org]["URL"]}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Org Key</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{alerts[import_org]["ORG_KEY"]}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Policy id</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{policy.id}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Policy Name</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{policy.name}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Priority Level</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{policy.priority_level}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Is System</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{policy.is_system}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Total Rules</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{len(policy.rules)}</dd>\n'
-    #     html += '            <dt class="col-sm-4">Description</dt>\n'
-    #     html += f'            <dd class="col-sm-8">{policy.description}</dd>\n'
-    #     html += '        </dl>\n'
-    #     html += f'       <a href="#" data-bs-toggle="modal" data-bs-target="#import_policy_{policy.id}_{policy.org_key}_modal">View Policy Details</a>'
-    #     html += f'        <div class="modal modal-xl fade" id="import_policy_{policy.id}_{policy.org_key}_modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'
-    #     html += '          <div class="modal-dialog modal-dialog-scrollable">'
-    #     html += '            <div class="modal-content">'
-    #     html += '              <div class="modal-header">'
-    #     html += '                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
-    #     html += '              </div>'
-    #     html += '              <div class="modal-body">'
-    #     html += f'                {generate_policy_html_table(policy._info)}'
-    #     html += '              </div>'
-    #     html += '            </div>'
-    #     html += '          </div>'
-    #     html += '        </div>'
-    #     html += '    </div>\n'
-    #     html += '    <div class="card-body p-0">\n'
-    #     html += '<div class="accordion accordion-flush" id="import_rules_accordion">'
-    #     html += '  <div class="accordion-item">'
-    #     html += '    <h2 class="accordion-header">'
-    #     html += f'      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#rules_{policy.id}_{policy.org_key}" aria-expanded="false" aria-controls="flush-collapseOne">'
-    #     html += '        Rules'
-    #     html += '      </button>'
-    #     html += '    </h2>'
-    #     html += f'    <div id="rules_{policy.id}_{policy.org_key}" class="accordion-collapse collapse">'
-    #     html += '      <div class="accordion-body">'
-    #     for rule in policy.rules:
-    #         html += '        <div class="row mx-3 align-items-center py-2 position-relative border-bottom border-200">\n'
-    #         html += '            <div class="col-1">\n'
-    #         html += '            </div>\n'
-    #         html += '            <div class="col py-1 position-static">\n'
-    #         html += '                <div class="d-flex align-items-center">\n'
-    #         html += '                    <dl class="row pt-3 lh-1 hr-margin-y">\n'
-    #         html += '                        <dt class="col-sm-4">Rule id</dt>\n'
-    #         html += f'                        <dd class="col-sm-8">{rule["id"]}</dd>\n'
-    #         html += '                        <dt class="col-sm-4">Required</dt>\n'
-    #         html += f'                        <dd class="col-sm-8">{rule["required"]}</dd>\n'
-    #         html += '                        <dt class="col-sm-4">Action</dt>\n'
-    #         html += f'                        <dd class="col-sm-8">{rule["action"]}</dd>\n'
-    #         html += '                        <dt class="col-sm-4">Operation</dt>\n'
-    #         html += f'                        <dd class="col-sm-8">{rule["operation"]}</dd>\n'
-    #         html += '                        <dt class="col-sm-4">Application</dt>\n'
-    #         html += '                        <dd class="col-sm-8">\n'
-    #         html += '                            <dl class="row">\n'
-    #         html += '                                <dt class="col-sm-6">type</dt>\n'
-    #         html += f'                                <dd class="col-sm-6">{rule["application"]["type"]}</dd>\n'
-    #         html += '                                <dt class="col-sm-6">value</dt>\n'
-    #         html += f'                                <dd class="col-sm-6">{rule["application"]["value"]}</dd>\n'
-    #         html += '                            </dl>\n'
-    #         html += '                        </dd>\n'
-    #         html += '                    </dl>\n'
-    #         html += '                </div>\n'
-    #         html += '            </div>\n'
-    #         html += '        </div>\n'
-    #     html += '            </div>'
-    #     html += '        </div>'
-    #     html += '    </div>'
-    #     html += '</div>'
-    #     html += '    </div>\n'
-    #     html += '</div>\n'
-    #     org_info["raw_html"] = html
+    for alert in alerts:
+        html += '<div class="card small mt-0 m-3 border">\n'
+        # html += '    <div class="card-header bg-light">\n'
+        # html += f'        <div class="modal modal-xl fade" id="import_policy_asf" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'
+        # html += '          <div class="modal-dialog modal-dialog-scrollable">'
+        # # html += '            <div class="modal-content">'
+        # # html += '              <div class="modal-header">'
+        # # html += '                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'
+        # # html += '              </div>'
+        # # html += '              <div class="modal-body">'
+        # # html += '                  <dl class="row lh-1 hr-margin-y">\n'
+        # # html += f'                     <dt class="col-sm-6">{alerts[alert]["type"]}</dt>\n'
+        # # html += f'                     <dd class="col-sm-6">asdasd</dd>\n'
+        # # html += '                  </dl>\n'
+        # # html += '              </div>'
+        # # html += '            </div>'
+        # html += '          </div>'
+        # html += '        </div>'
+        # html += '    </div>\n'
+        html += '    <div class="card-body p-3">\n'
+        html += '        <dl class="row lh-1 hr-margin-y">\n'
+        html += '            <dt class="col-sm-3">Alert ID</dt>\n'
+        html += f'            <dd class="col-sm-7">{alert}</dd>\n'
+        for item in alerts[alert]:
+            html += f'<dt class="col-sm-3">{item.capitalize()}</dt>\n'
+            html += f'<dd class="col-sm-7">{alerts[alert][item]}</dd>\n'
+        html += '        </dl>\n'
+        html += f'       <a href="#" data-bs-toggle="modal" data-bs-target="#import_policyasfasfsaf_modal">Details</a>'
+        html += '    </div>\n'
+        html += '</div>\n'
+        # org_info["raw_html"] = html
 
     # org_info["raw_html"] = html
     # org_info["data_timestamp"] = str(datetime.datetime.now())
     # org_info["org_keys"] = ', '.join(alerts.keys())
     # org_info["orgs_count_total"] = len(alerts.keys())
 
-    # return json.dumps(org_info)
+    return json.dumps(html)
 
 
 if __name__ == "__main__":
--- carbon_alert_matcher/web/main.js
+++ carbon_alert_matcher/web/main.js
@@ -15,44 +15,6 @@ function get_export_settings() {
 		import_settings["policy_name_prefix"] = document.getElementById("import_policy_name_prefix").value
 }
 
-async function delete_org(org_type, org_id) {
-		if (org_type == "export_org") {
-				var org_key = document.getElementById("ExportOrgKey").value;
-				document.getElementById("ExportURL").value = "";
-				document.getElementById("ExportApiId").value = "";
-				document.getElementById("ExportOrgKey").value = "";
-				document.getElementById("ExportApiSecretKey").value = "";
-				if (org_key != "") {
-						await eel.delete_org(org_type, org_key)();
-						export_org_data_set = false;
-						export_org_raw_data = {};
-						get_export_org_info();
-				}
-		} else if (org_type == "import_org") {
-				// var org_key = document.getElementById("ExportOrgKey").value;
-				document.getElementById("ImportURL_"  + org_id).value = "";
-				document.getElementById("apiId_" + org_id).value = "";
-				document.getElementById("ImportOrgKey_" + org_id).value = "";
-				document.getElementById("apiSecretKey_" + org_id).value = "";
-				if (org_id != "") {
-						await eel.delete_org(org_type, org_id)();
-						let refresh_info = true;
-						import_org_data_set = false;
-						// import_data_selected = false;
-						// import_settings = {}
-						get_import_orgs_info(refresh_info);
-				}
-		}
-		await eel.read_config()();
-		enable_import_export();
-}
-
-async function add_new_org() {
-		let data = await eel.add_new_org()();
-		var new_html = data + document.getElementById("importOrgsSettingsModalPlaceholder").innerHTML;
-		document.getElementById("importOrgsSettingsModalPlaceholder").innerHTML = new_html;
-}
-
 async function save_import_orgs_settings() {
 		var data = document.querySelectorAll(".newOrg");
 		var new_orgs = {}
@@ -70,22 +32,6 @@ async function save_import_orgs_settings() {
 		await eel.read_config()();
 		let new_data = await eel.refresh_org_data('import')();
 		refresh_org_data(JSON.parse(new_data), 'import');
-}
-
-async function save_export_org_settings() {
-		var new_org = {}
-		var org_key = document.getElementById("ExportOrgKey").value
-		new_org[org_key] = {
-			"URL": document.getElementById("ExportURL").value,
-			"API_ID": document.getElementById("ExportApiId").value,
-			"API_SECRET": document.getElementById("ExportApiSecretKey").value,
-			"ORG_KEY": document.getElementById("ExportOrgKey").value,
-			// "NOTES": document.getElementById("new_notes_" + org.id).value
-		}
-		await eel.save_org_data(new_org, 'export_org')();
-		await eel.read_config()();
-		let new_data = await eel.refresh_org_data('export')();
-		refresh_org_data(JSON.parse(new_data), 'export');
 }
 
 async function refresh_org_data(data, type) {
@@ -112,21 +58,21 @@ async function refresh_org_data(data, type) {
 						policyInfoBtnStat = true;
 						raw_html = "<div class='alert alert-warning m-3 text-center' role='alert'>Import Orgs not configured.</div>"
 				} else {
-						org_keys = data.org_keys;
+						org_keys = 'asd';
 						orgs_count_total = data.orgs_count_total;
 						policyInfoBtnStat = false;
-						raw_html = data.raw_html;
+						raw_html = data;
 				}
-				document.getElementById("import_meta_org_keys").textContent = org_keys;
-				document.getElementById("import_meta_orgs_count_total").textContent = orgs_count_total;
-				document.getElementById("import_meta_timestamp").textContent = data.data_timestamp;
-				document.getElementById("import_meta_rule_count_total").textContent = data.rule_count_total;
-				document.getElementById("import_meta_policy_count_total").textContent = data.policy_count_total;
+				// document.getElementById("import_meta_org_keys").textContent = org_keys;
+				// document.getElementById("import_meta_orgs_count_total").textContent = orgs_count_total;
+				// document.getElementById("import_meta_timestamp").textContent = data.data_timestamp;
+				// document.getElementById("import_meta_rule_count_total").textContent = data.rule_count_total;
+				// document.getElementById("import_meta_policy_count_total").textContent = data.policy_count_total;
 				document.getElementById("importPolicyContainer").innerHTML = raw_html;
 				document.getElementById("getImportPolicyInfoBtn").disabled = policyInfoBtnStat;
 				document.getElementById("getImportPolicyInfoBtnSpinner").classList.add("d-none");
 		}
-		enable_import_export();
+		// enable_import_export();
 }
 
 async function print_import_data() {
@@ -149,15 +95,6 @@ async function import_data() {
 		document.getElementById("importOrgsCard").classList.remove("disableddiv");
 }
 
-function enable_import_export() {
-		if (export_org_data_set == true && Object.keys(export_data).length > 0 && import_org_data_set == true) {
-				document.getElementById("importDataBtn").disabled = false;
-				// document.getElementById("compareDataBtn").disabled = false;
-		} else {
-				document.getElementById("importDataBtn").disabled = true;
-				// document.getElementById("compareDataBtn").disabled = true;
-		}
-}
 
 function select_policy(export_type, policy_id, rule_id) {
 		if (rule_id) {
@@ -198,54 +135,29 @@ function select_policy(export_type, policy_id, rule_id) {
 						export_data[policy_id] = arrayRemove(export_data[policy_id], rule_id);
 				}
 		}
-		enable_import_export();
-}
-
-async function get_export_org_settings() {
-		let data = await eel.export_org_settings()();
-		let secret_key = "";
-		var credentials = JSON.parse(data);
-		if (credentials.api_secret_key != "") {
-			secret_key = "*************************"
-		}
-		document.getElementById("ExportURL").value = credentials.url;
-		document.getElementById("ExportApiId").value = credentials.api_id;
-		document.getElementById("ExportApiSecretKey").value = secret_key;
-		document.getElementById("ExportOrgKey").value = credentials.org_key;
-		if (credentials.org_key != "") {
-				document.getElementById("ExportOrgDeleteOrgBtn").classList.remove("d-none");
-				document.getElementById("ImportOrgSettingsForm").disabled = true;
-				document.getElementById("ImportOrgSettingsSaveBtn").classList.add("d-none");
-		} else {
-				document.getElementById("ExportOrgDeleteOrgBtn").classList.add("d-none");
-				document.getElementById("ImportOrgSettingsForm").disabled = false;
-				document.getElementById("ImportOrgSettingsSaveBtn").classList.remove("d-none");
-		}
-}
-
-async function get_import_orgs_settings() {
-		let data = await eel.import_orgs_settings()();
-		document.getElementById("importOrgsSettingsModalPlaceholder").innerHTML = data;
+		// enable_import_export();
 }
 
 async function get_alerts() {
 		document.getElementById("getPolicyInfoBtn").disabled = true;
 		document.getElementById("getPolicyInfoBtnSpinner").classList.remove("d-none");
-		let org_data = await eel.get_alerts()();
+		var match_similar = document.getElementById("matchSimilar").checked;
+		var cb_analytics = document.getElementById("cbAnalitycs").checked;
+		var watchlists = document.getElementById("watchlists").checked;
+		var usb_device_control = document.getElementById("usbDeviceControl").checked;
+		var host_based_firewall = document.getElementById("hostBasedFirewall").checked;
+		var intrusion_detection_system = document.getElementById("intrusionDetectionSystem").checked;
+		var containers_runtime = document.getElementById("containersRuntime").checked;
+		let org_data = await eel.get_alerts(match_similar, cb_analytics, watchlists, usb_device_control, host_based_firewall, intrusion_detection_system, containers_runtime)();
 		var parsed_data = JSON.parse(org_data);
+		console.log(parsed_data)
 		if (parsed_data) {
-				if (parsed_data.org_key == "-") {
-						export_org_data_set = false;
-						export_data = {};
-						export_org_raw_data = {};
-						document.getElementById("getPolicyInfoBtn").disabled = true;
-				} else {
-						export_org_data_set = true;
-						export_org_raw_data = parsed_data.raw_data;
-						document.getElementById("getPolicyInfoBtn").disabled = false;
-				}
-				refresh_org_data(parsed_data, 'export');
-				enable_import_export();
+			export_org_data_set = true;
+			// export_org_raw_data = parsed_data.raw_data;
+			export_org_raw_data = parsed_data;
+			document.getElementById("getPolicyInfoBtn").disabled = false;
+			refresh_org_data(parsed_data, 'import');
+			// enable_import_export();
 		}
 }
 
@@ -262,5 +174,5 @@ async function get_import_orgs_info(refresh_info) {
 				let new_data = await eel.refresh_org_data('import')();
 				refresh_org_data(JSON.parse(new_data), 'import');
 		}
-		enable_import_export();
+		// enable_import_export();
 }
